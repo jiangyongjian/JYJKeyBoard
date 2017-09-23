ce948199bad9442fc6daf98bd97ca1a8fde66c2a
JYJ <542829817@qq.com>
2017-09-23 16:58:41 +0800

    适配iOS 11
    适配iOS 11

--- JYJKeyBoard/JYJKeyBoard/JYJKeyBoard/BXTextField.m
+++ JYJKeyBoard/JYJKeyBoard/JYJKeyBoard/BXTextField.m
@@ -10,6 +10,10 @@
 #import "UIView+BXExtension.h"
 #import "UITextField+BXExtension.h"
 
+// 是否为iPhoneX
+#define iPhoneX ([UIScreen mainScreen].bounds.size.height == 812)
+#define BXDangerousAreaH 34
+
 @interface BXTextField ()
 /** X按钮 */
 @property (nonatomic, strong) UIButton *doneButton;
@@ -17,6 +21,10 @@
 @property (nonatomic, assign) BOOL willShowKeyboard;
 
 @property (nonatomic, assign) BOOL displayingKeyboard;
+
+/** notification */
+@property (nonatomic, strong) NSNotification *notification;
+
 @end
 
 @implementation BXTextField
@@ -50,12 +58,21 @@
 - (void)keyboardWillBeginShow:(NSNotification *)notification {
     if (self.keyboardType != UIKeyboardTypeNumberPad) return;
     self.willShowKeyboard = notification.object == self;
+//    NSLog(@"%@ --- %zd", notification, self.willShowKeyboard);
+    if (self.willShowKeyboard) {
+        // 发出通知
+        if (@available(iOS 11.0, *)) {
+            if (self.notification) {
+                [self setupDownKey];
+            }
+        }
+    }
 }
-
 
 - (void)keyboardWillEndShow:(NSNotification *)notification {
     if (self.keyboardType != UIKeyboardTypeNumberPad) return;
     self.willShowKeyboard = NO;
+    
     NSDictionary *userInfo = [notification userInfo];
     CGFloat animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
     NSInteger animationCurve = [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
@@ -71,20 +88,6 @@
 
 - (void)keyboardWillShow:(NSNotification *)notification {
     if (self.keyboardType != UIKeyboardTypeNumberPad) return;
-    if (!self.willShowKeyboard) {
-        self.displayingKeyboard = YES;
-        return;
-    }
-    NSLog(@"%zd", self.displayingKeyboard);
-    [self.doneButton removeFromSuperview];
-    self.doneButton = nil;
-
-    NSDictionary *userInfo = [notification userInfo];
-    // 动画时间
-    CGFloat animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
-    // 键盘的Frame
-    CGRect kbEndFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    CGFloat kbHeight = kbEndFrame.size.height;
     
     // 获取到最上层的window,这句代码很关键
     UIWindow *tempWindow = [[[UIApplication sharedApplication] windows] objectAtIndex:1];
@@ -99,11 +102,33 @@
         if (![UIView ff_foundViewInView:tempWindow clazzName:@"UIKeyboardAutomatic"]) return;
     }
     
+    self.notification = notification;
+    [self setupDownKey];
+}
+
+- (void)setupDownKey {
+    if (!self.willShowKeyboard) {
+        self.displayingKeyboard = YES;
+        return;
+    }
+//    NSLog(@"%zd", self.displayingKeyboard);
+    [self.doneButton removeFromSuperview];
+    self.doneButton = nil;
+    
+    NSDictionary *userInfo = [self.notification userInfo];
+
+    // 动画时间
+    CGFloat animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    // 键盘的Frame
+    CGRect kbEndFrame = [self.notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat kbHeight = kbEndFrame.size.height;
+    
+//    NSLog(@"%f -- 291 --- 216", kbHeight);
     // 这里因为用了第三方的键盘顶部，所有加了44
     if (self.adjustTextFeildH) {
         kbHeight = kbEndFrame.size.height - 44;
     }
-
+    
     // 动画的轨迹
     NSInteger animationCurve = [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
     
@@ -128,13 +153,34 @@
     } else {
         doneButtonY = [UIScreen mainScreen].bounds.size.height + kbHeight - doneButtonH;
     }
+    if (iPhoneX) {
+        doneButtonH = (kbHeight - 75 - 2) / 4;
+        if (self.displayingKeyboard) {
+            doneButtonY = [UIScreen mainScreen].bounds.size.height - doneButtonH;
+        } else {
+            doneButtonY = [UIScreen mainScreen].bounds.size.height + kbHeight - doneButtonH;
+        }
+        doneButtonY -= 75;
+    }
     UIButton *doneButton = [[UIButton alloc] initWithFrame:CGRectMake(doneButtonX, doneButtonY, doneButtonW, doneButtonH)];
+    
     doneButton.titleLabel.font = [UIFont systemFontOfSize:27];
     [doneButton setTitle:@"X" forState:(UIControlStateNormal)];
     [doneButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [doneButton setBackgroundImage:[self createImageWithColor:[UIColor whiteColor]] forState:UIControlStateHighlighted];
+    if (@available(iOS 11.0, *)) {
+        if (self.displayingKeyboard) {
+            doneButton.alpha = 0.0;
+            [UIView animateWithDuration:0.1 animations:^{
+                doneButton.alpha = 1.0;
+            }];
+        }
+    } else {
+        [doneButton setBackgroundImage:[self createImageWithColor:[UIColor whiteColor]] forState:UIControlStateHighlighted];
+    }
     [doneButton addTarget:self action:@selector(doneButton:) forControlEvents:UIControlEventTouchUpInside];
     self.doneButton = doneButton;
+    // 获取到最上层的window,这句代码很关键
+    UIWindow *tempWindow = [[[UIApplication sharedApplication] windows] lastObject];
     // 添加按钮
     [tempWindow addSubview:doneButton];
     
@@ -148,11 +194,13 @@
         [UIView commitAnimations];
     }
     self.displayingKeyboard = YES;
+    
 }
 
 - (void)keyboardWillHide:(NSNotification *)notification {
     if (self.keyboardType != UIKeyboardTypeNumberPad) return;
     self.displayingKeyboard = NO;
+    self.notification = nil;
 }
 
 #pragma mark - 私有方法
